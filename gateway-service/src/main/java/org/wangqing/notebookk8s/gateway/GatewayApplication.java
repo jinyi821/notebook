213e9b39402e973ea5d3a3d1c14a031bed337244
jinyi821 <21990173@qq.com>
2022-09-07 15:52:09 +0800

    commit

--- gateway-service/src/main/java/org/wangqing/notebookk8s/gateway/GatewayApplication.java
+++ gateway-service/src/main/java/org/wangqing/notebookk8s/gateway/GatewayApplication.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GatewayApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
